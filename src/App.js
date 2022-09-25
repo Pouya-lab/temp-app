b85f7b548dd3ba100420fa7c522a8f8f33fe909f
pouya <pouyabh102@yahoo.com>
2022-09-25 15:23:03 +0330

    html element are compeleted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./style.css";
 
 function App() {
+  const [count , setCount] = useState(10)
+
   return (
-    <div>App</div>
+    <>
+     <section className="container d-flex">
+     <section className="app d-flex">
+      <section className='show'>
+        <div className="temp">{count}°C</div>
+        <section className="btn-gp d-flex">
+            <button className="btn">-</button>
+            <button className="btn">+</button>
+        </section>
+      </section>
+     </section> 
+     </section>
+    </>
   )
 }
 
