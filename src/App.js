6e3dd152de1813337dcba9b6ec368b82b309bc42
pouya <pouyabh102@yahoo.com>
2022-09-26 12:34:03 +0330

    functions and states were added and app is done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import React, { useState } from 'react';
 import "./style.css";
 
 function App() {
-  const [count , setCount] = useState(10)
+  
+  const [tempValue , setTempValue] = useState(15)
+ const [tempColor , setTempColor] = useState('default-color')
+
+ const tempIncrease = () =>{
+  setTempValue(tempValue + 1)
+  if(tempValue >= 15){
+    setTempColor('hot')
+  }
+
+ }
+
+ const tempDecrease = () =>{
+  setTempValue(tempValue - 1)
+  if (tempValue <= 16) {
+    setTempColor('cold')
+  }
+
+ }
 
   return (
     <>
+     <h1 className="name d-flex">Temperture Control</h1>
      <section className="container d-flex">
      <section className="app d-flex">
       <section className='show'>
-        <div className="temp">{count}°C</div>
+        <div className={`temp ${tempColor}`}>{tempValue}°C</div>
         <section className="btn-gp d-flex">
-            <button className="btn">-</button>
-            <button className="btn">+</button>
+            <button className="btn" onClick={tempDecrease} >-</button>
+            <button className="btn" onClick={tempIncrease} >+</button>
+            <button className="btn" onClick={()=>{setTempValue(15) ; setTempColor('default-color') } } >Reset</button>
         </section>
       </section>
      </section> 
