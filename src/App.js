423699e891af43ba8f59fb02bc8522fa901d0bdc
pouya <pouya_behrooj@yahoo.com>
2023-01-05 16:49:18 +0330

    updated

--- src/App.js
+++ src/App.js
@@ -27,13 +27,13 @@ function App() {
      <h1 className="name d-flex">Temperture Control</h1>
      <section className="container d-flex">
      <section className="app d-flex">
-      <section className='show'>
-        <div className={`temp ${tempColor}`}>{tempValue}°C</div>
-        <section className="btn-gp d-flex">
-            <button className="btn" onClick={tempDecrease} >-</button>
-            <button className="btn" onClick={tempIncrease} >+</button>
-            <button className="btn" onClick={()=>{setTempValue(15) ; setTempColor('default-color') } } >Reset</button>
-        </section>
+        <section className='show'>
+          <div className={`temp ${tempColor}`}>{tempValue}°C</div>
+          <section className="d-flex">
+              <button className="btn" onClick={tempDecrease} >-</button>
+              <button className="btn" onClick={tempIncrease} >+</button>
+              <button className="btn" onClick={()=>{setTempValue(15) ; setTempColor('default-color') } } >Reset</button>
+          </section>
       </section>
      </section> 
      </section>
